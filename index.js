f61c606d2f87997ef1bd699bd2b45ec23c975566
Jason Villalon <jasonvillalon@gmail.com>
2015-12-14 20:01:30 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from "react"
+
+import dependencies from "./dependencies"
+
+const IonView = React.createClass({
+  render() {
+    return (
+      <div className="ionview ">
+        Ion view
+      </div>
+    )
+  }
+})
+
+export default IonView
---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Name: "IonView",
+  Description: "Ion view",
+  Repository: "git@bitbucket.org:ionicreact/ion-view.git",
+  AtomicDeps: []
+};
