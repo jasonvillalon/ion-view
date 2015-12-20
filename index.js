d9d747da3265f958f6d494e2984a0859e9e135bf
Jason Villalon <jasonvillalon@gmail.com>
2015-12-20 09:34:55 +0800

    Fixing some bugs...

--- index.js
+++ index.js
@@ -1,70 +1,35 @@
 import React, {PropTypes} from "react"
 import history from "./../history"
 import ReactCSSTransitionGroup from "react-addons-css-transition-group"
-import StaticContainer from "react-static-container"
-
 import dependencies from "./dependencies"
 
 let {Layout} = dependencies
-
-class RouteCSSTransitionGroup extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-
-    this.state = {
-      previousPathname: null
-    }
-  }
-
-  componentWillReceiveProps(nextProps, nextContext) {
-    if (nextContext.location.pathname !== this.context.location.pathname) {
-      this.setState({previousPathname: this.context.location.pathname})
-    }
-  }
-
-  render() {
-    const { children, ...props } = this.props
-    const { previousPathname } = this.state
-
-    return (
-      <ReactCSSTransitionGroup {...props}>
-        <StaticContainer
-          key={previousPathname || this.context.location.pathname}
-          shouldUpdate={!previousPathname}
-        >
-          {children}
-        </StaticContainer>
-      </ReactCSSTransitionGroup>
-    )
-  }
-
-  componentDidUpdate() {
-    if (this.state.previousPathname) {
-      this.setState({previousPathname: null})
-    }
-  }
-}
-
-
-RouteCSSTransitionGroup.contextTypes = {
-  location: React.PropTypes.object
-}
 
 const IonView = React.createClass({
   mixins: [Layout],
   getInitialState: function() {
     return {
       direction: "right",
-      paths: []
+      paths: [],
+      style: {},
+      animatingCls: "",
+      windowWidth: window.innerWidth
     }
+  },
+  onResize(dimention) {
+    this.setState({
+      windowWidth: dimention.innerWidth
+    })
   },
   componentDidMount() {
     history.listen(this.historyListener)
   },
   componentWillUnmount() {
     history.unlisten(this.historyListener)
+    console.log("unmounting")
   },
   historyListener(current) {
+    console.log(current)
     if (current.action === "PUSH") {
       this.state.paths.push(current)
       this.state.direction = "right"
@@ -73,17 +38,29 @@ const IonView = React.createClass({
       this.state.paths.splice(this.state.paths.length - 1, 1)
     }
     this.setState({
-      paths: this.state.paths,
-      direction: this.state.direction
+      paths: this.state.paths
     })
+    this.setState({
+      paths: this.state.paths,
+      animatingCls: "",
+      style: {
+        transform: `translateX(${((this.state.windowWidth / 2) * (this.state.direction == "right" ? 1 : -1))}px) translateZ(0px)`
+      }
+    })
+    setTimeout(() => {
+      this.setState({
+        animatingCls: " animating",
+        style: {
+          transform: "translateX(0px) translateZ(0px)"
+        }
+      })
+    }, 100)
   },
   render() {
     return (
-      <RouteCSSTransitionGroup {...this.layout("row")} {...this.flex()} {...this.fill} component="div" transitionName={this.state.direction} transitionEnterTimeout={500} transitionLeaveTimeout={300}>
-        <div className="ionview" {...this.flex()} {...this.fill}>
-          {this.props.children}
-        </div>
-      </RouteCSSTransitionGroup>
+      <div className={"ion-view" + this.state.animatingCls} style={this.state.style} {...this.fill} {...this.flex()} {...this.layout("row")}>
+        {this.props.children}
+      </div>
     )
   }
 })
